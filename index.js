8b174b1476fc0fe6fe1eac816fa60c2f9b32fee1
Muhammed Ameer <108969040+MhDAmeer@users.noreply.github.com>
2023-04-11 14:26:24 +0530

    added html,css,javascript for scoreboard

---
+++ index.js
@@ -0,0 +1,15 @@
+let homeScore= document.getElementById("home-score");
+let guestScore= document.getElementById("guest-score");
+
+let scoreHome = 0;
+let scoreGuest = 0;
+
+function addPoint(value, type) {
+  if (type === 'home') {
+    scoreHome += value;
+    homeScore.innerHTML = scoreHome;
+  } else {
+    scoreGuest += value;
+    guestScore.textContent = scoreGuest;
+  }
+}
